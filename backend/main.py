de3afc81b234b36577316182a21230f71b9d2151
Aryan Mestry <aryanmestry07@gmail.com>
2026-08-26 17:22:59 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://support-crm-api-zwvp.onrender.com"
+        "https://support-crm-api-zwvp.onrender.com",
+        "https://support-crm-azure.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
